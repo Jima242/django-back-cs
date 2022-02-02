802b27ff9cf5e01f78fd6f1d9c1d87326b489b00
Jima242 <193268@ids.upchiapas.edu.mx>
2022-02-02 03:01:51 -0600

    add two models in component

--- primerApp/settings.py
+++ primerApp/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #Componentes agregados
+    'primerComponente',
+
     #Librerias agregadas al pro 
     # yecto
     'rest_framework',
